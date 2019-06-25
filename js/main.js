04e5400830909b8cfdd74009cb6152827b1ef186
mortanbor <42118390+mortanbor@users.noreply.github.com>
2019-06-26 02:16:03 +0300

    всё еще первое задание

--- js/main.js
+++ js/main.js
@@ -7,7 +7,8 @@ var HEIGHT_MAP_START = 130;
 var HEIGHT_MAP_FINISH = 630;
 var WIDTH_PIN = 40;
 var HEIGHT_PIN = 44;
-var widthMap = map.offsetWidth;
+var widthMap = 1500;
+/* map.offsetWidth; */
 
 // у блока .map убираю класс .map--faded
 var map = document.querySelector('.map');
@@ -26,7 +27,7 @@ var getRandomInteger = function (min, max) {
 
 // получаем непосредственно значение элемента массива по случайно полученному индексу
 var getRandomElement = function (elements) {
-  return  elements[getRandomInteger(0, elements.length)];
+  return elements[getRandomInteger(0, elements.length)];
 };
 
 // формируем массив из  восьми JS объектов
@@ -42,28 +43,28 @@ var getPinsDescription = function () {
         type: getRandomElement(TYPES)
       },
       location: {
-        x: getRandomInteger(0, WIDTH_MAP),
+        x: getRandomInteger(0, widthMap),
         y: getRandomInteger(HEIGHT_MAP_START, HEIGHT_MAP_FINISH)
       }
     });
-   return pins;
- };
-}
+  }
+  return pins;
+};
 
 // создание DOM-элемента, заполнение его данными из массива getPinsDescription
- var renderPin = function (pin) {
-   var pinElement = similarTemplateElement.cloneNode(true); // находим шаблон для копирования и копируем его полностью
-   var imgPin = pinElement.querySelector('.img');
-   var buttonPin = pinElement.querySelector('.map__pin');
+var renderPin = function (pin) {
+  var pinElement = similarTemplateElement.cloneNode(true); // находим шаблон для копирования и копируем его полностью
+  var imgPin = pinElement.querySelector('.img');
+  var buttonPin = pinElement.querySelector('.map__pin');
 
-   imgPin.imgPin.src = pin.author.avatar;
-   imgPin.imgPin.alt = pin.offer.type;
-   buttonPin.buttonPin.style = pin.location.x;
-   buttonPin.buttonPin.style = pin.location.y;
- };
+  imgPin.imgPin.src = pin.author.avatar;
+  imgPin.imgPin.alt = pin.offer.type;
+  buttonPin.buttonPin.style = pin.location.x - (WIDTH_PIN - (WIDTH_PIN / 2));
+  buttonPin.buttonPin.style = pin.location.y - (HEIGHT_PIN - (HEIGHT_PIN / 2));
+};
 
- // отрисовка сгенерированных DOM-элементов в блок .map__pins с использованим DocumentFragment
- var collectFragment = function (pins) {
+// отрисовка сгенерированных DOM-элементов в блок .map__pins с использованим DocumentFragment
+var collectFragment = function (pins) {
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < pins.length; i++) {
     fragment.appendChild(renderPin(pins[i]));
