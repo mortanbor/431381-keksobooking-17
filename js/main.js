e39035cc08ea0ef17ff962547a8b719951edd7a0
mortanbor <42118390+mortanbor@users.noreply.github.com>
2019-06-25 17:39:14 +0300

    Кексобукинг

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@
 // статичные исходные данные
 var TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var NUMBERS_OF_PINS = 8;
-var WIDTH_MAP = map.offsetWidth;
 var HEIGHT_MAP_START = 130;
 var HEIGHT_MAP_FINISH = 630;
 var WIDTH_PIN = 40;
 var HEIGHT_PIN = 44;
+var widthMap = map.offsetWidth;
 
 // у блока .map убираю класс .map--faded
 var map = document.querySelector('.map');
@@ -17,17 +17,16 @@ map.classList.remove('map--faded');
 // первая строка - это то, куда будем копировать
 // вторая строка - откуда берём шаблон для копирования
 var similarListElement = document.querySelector('.map__pins');
-var similarPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+var similarTemplateElement = document.querySelector('#pin').content.querySelector('.map__pin');
 
 // получаем случайный номер элемента. Элемент массива или данные - вставляем аргументом в функции
-var getRandomIndex = function (min, max) {
-  var rand;
-  return rand = Math.floor((min + Math.random()) * (max - min) * arr.length);
+var getRandomInteger = function (min, max) {
+  return Math.floor(min + Math.random()) * (max - min);
 };
 
 // получаем непосредственно значение элемента массива по случайно полученному индексу
-var getRandomElement = function (arr) {
-  return arr[getRandomIndex(0, arr.length)];
+var getRandomElement = function (elements) {
+  return  elements[getRandomInteger(0, elements.length)];
 };
 
 // формируем массив из  восьми JS объектов
@@ -37,14 +36,14 @@ var getPinsDescription = function () {
   for (var i = 0; i < NUMBERS_OF_PINS; i++) {
     pins.push({
       author: {
-        avatar: 'img/avatars/user' + i + '.png',
+        avatar: 'img/avatars/user0' + i + '.png'
       },
       offer: {
         type: getRandomElement(TYPES)
       },
       location: {
-        x: getRandomIndex(0, WIDTH_MAP),
-        y: getRandomIndex(HEIGHT_MAP_START, HEIGHT_MAP_FINISH)
+        x: getRandomInteger(0, WIDTH_MAP),
+        y: getRandomInteger(HEIGHT_MAP_START, HEIGHT_MAP_FINISH)
       }
     });
    return pins;
@@ -52,18 +51,18 @@ var getPinsDescription = function () {
 
 // создание DOM-элемента, заполнение его данными из массива getPinsDescription
  var renderPin = function (pin) {
-   var PinElement = similarPinTemplate.cloneNode(true); // находим шаблон для копирования и копируем его полностью
-   var imgPin = document.querySelector('.img');
-   var buttonPin = document.querySelector('.map__pin');
+   var pinElement = similarTemplateElement.cloneNode(true); // находим шаблон для копирования и копируем его полностью
+   var imgPin = pinElement.querySelector('.img');
+   var buttonPin = pinElement.querySelector('.map__pin');
 
-   PinElement.imgPin.src = pin.author.avatar;
-   PinElement.imgPin.alt = pin.alt;
-   PinElement.buttonPin.style = pin.location.x - (WIDTH_PIN / 2);
-   PinElement.buttonPin.style = pin.location.y - (HEIGHT_PIN / 2);
+   imgPin.imgPin.src = pin.author.avatar;
+   imgPin.imgPin.alt = pin.offer.type;
+   buttonPin.buttonPin.style = pin.location.x;
+   buttonPin.buttonPin.style = pin.location.y;
  };
 
  // отрисовка сгенерированных DOM-элементов в блок .map__pins с использованим DocumentFragment
- var collectFragment = function (NUMBERS_OF_PINS) {
+ var collectFragment = function (pins) {
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < pins.length; i++) {
     fragment.appendChild(renderPin(pins[i]));
@@ -71,4 +70,4 @@ var getPinsDescription = function () {
   similarListElement.appendChild(fragment);
 };
 
-collectFragment (getPinsDescription());
+collectFragment(getPinsDescription());
