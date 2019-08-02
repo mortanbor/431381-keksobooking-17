bc03da23512496b9756a99ebee591acf280dd573
mortanbor <42118390+mortanbor@users.noreply.github.com>
2019-08-02 22:22:34 +0300

    form

--- js/map.js
+++ js/map.js
@@ -2,10 +2,14 @@
 
 (function () {
   var map = document.querySelector('.map');
+  var filtersBlock = map.querySelector('.map__filters');
+  var filtersNodes = filtersBlock.querySelectorAll('.map__filter');
   var widthMap = map.offsetWidth;
 
   window.map = {
     node: map,
+    filtersBlock: filtersBlock,
+    filtersNodes: filtersNodes,
     width: widthMap
   };
 })();
--- js/pins.js
+++ js/pins.js
@@ -1,39 +1,38 @@
 'use strict';
 
 (function () {
-  var mapFilters = window.map.node.querySelectorAll('.map__filter');
-
   var widthPinHalf = window.data.PIN_WIDTH / 2;
   var heightPinHalf = window.data.PIN_HEIGHT / 2;
 
   // формируем шаблон для копирования
   // первая строка - это то, куда будем копировать
   // вторая строка - откуда берём шаблон для копирования
-  var similarListElement = window.map.node.querySelector('.map__pins');
+  var pinsContainer = window.map.node.querySelector('.map__pins');
   var similarTemplateElement = document.querySelector('#pin').content.querySelector('.map__pin');
 
-  // формируем массив из  восьми JS объектов
-  var getPinsDescription = function () {
-    var pins = [];
+  // // формируем массив из  восьми JS объектов
+  // удаляем перед защитой, использовала для формирования массива из моковых данных
+  // var getPinsDescription = function () {
+  //   var pins = [];
+  //
+  //   for (var i = 1; i <= window.data.NUMBERS_OF_PINS; i++) {
+  //     pins.push({
+  //       author: {
+  //         avatar: 'img/avatars/user0' + i + '.png'
+  //       },
+  //       offer: {
+  //         type: window.utils.getRandomElement(window.data.TYPES)
+  //       },
+  //       location: {
+  //         x: window.utils.getRandomInteger(widthPinHalf, window.map.width - widthPinHalf),
+  //         y: window.utils.getRandomInteger(window.data.MIN_MAP_Y, window.data.MAX_MAP_Y)
+  //       }
+  //     });
+  //   }
+  //   return pins;
+  // };
 
-    for (var i = 1; i <= window.data.NUMBERS_OF_PINS; i++) {
-      pins.push({
-        author: {
-          avatar: 'img/avatars/user0' + i + '.png'
-        },
-        offer: {
-          type: window.utils.getRandomElement(window.data.TYPES)
-        },
-        location: {
-          x: window.utils.getRandomInteger(widthPinHalf, window.map.width - widthPinHalf),
-          y: window.utils.getRandomInteger(window.data.MIN_MAP_Y, window.data.MAX_MAP_Y)
-        }
-      });
-    }
-    return pins;
-  };
-
-  // создание DOM-элемента, заполнение его данными из массива getPinsDescription
+  // создание DOM-элемента, заполнение его данными из массива объявлений
   var renderPin = function (pin) {
     var pinElement = similarTemplateElement.cloneNode(true); // находим шаблон для копирования и копируем его полностью
     var imgPin = pinElement.querySelector('img');
@@ -48,17 +47,22 @@
 
   // отрисовка сгенерированных DOM-элементов в блок .map__pins с использованим DocumentFragment
   var collectFragment = function (pins) {
+    similarListElement.innerHTML = '';
     var fragment = document.createDocumentFragment();
-    for (var i = 0; i < pins.length; i++) {
+    var limit = Math.min(pins.length, window.data.NUMBERS_OF_PINS);
+    for (var i = 0; i < limit; i++) {
       fragment.appendChild(renderPin(pins[i]));
     }
     similarListElement.appendChild(fragment);
   };
 
-  window.utils.setElementsDisabled(mapFilters);
+  var similarListElement = document.createElement('div');
+  pinsContainer.appendChild(similarListElement);
+
+  window.utils.setElementsDisabled(window.map.filtersNodes);
 
   window.pins = {
-    mapFilters: mapFilters,
+    data: [], // объявляем массив данных, куда записывать данные с сервера
     collectFragment: function (pins) {
       collectFragment(pins);
     }
